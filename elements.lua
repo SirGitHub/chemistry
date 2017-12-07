04a3b4960e68ca5871dc207539f1996771e1abe1
archfan <33993466+archfan7411@users.noreply.github.com>
2017-12-07 13:27:16 -0500

    Added first 10 elements

---
+++ elements.lua
@@ -0,0 +1,59 @@
+minetest.register_craftitem("chemistry:hydrogen", {
+	description = "Hydrogen: 1 VE, 1 P, 2 N",
+	inventory_image = "chemistry_hydrogen.png",
+	
+})
+
+minetest.register_craftitem("chemistry:helium", {
+	description = "Helium: 2 VE, 2 P, 2 N",
+	inventory_image = "chemistry_helium.png",
+	
+})
+
+minetest.register_craftitem("chemistry:lithium", {
+	description = "Lithium: 1 VE, 3 P, 4 N",
+	inventory_image = "chemistry_hydrogen.png",
+	
+})
+
+minetest.register_craftitem("chemistry:berillium", {
+	description = "Berillium: 2 VE, 4 P, 4 N",
+	inventory_image = "chemistry_berillium.png",
+	
+})
+
+minetest.register_craftitem("chemistry:boron", {
+	description = "Boron: 3 VE, 5 P, 6 N",
+	inventory_image = "chemistry_boron.png",
+	
+})
+
+minetest.register_craftitem("chemistry:carbon", {
+	description = "Carbon: 4 VE, 6 P, 7 N",
+	inventory_image = "chemistry_carbon.png",
+	
+})
+
+minetest.register_craftitem("chemistry:nitrogen", {
+	description = "Nitrogen: 5 VE, 7 P, 8 N",
+	inventory_image = "chemistry_nitrogen.png",
+	
+})
+
+minetest.register_craftitem("chemistry:oxygen", {
+	description = "Oxygen: 6 VE, 8 P, 9 N",
+	inventory_image = "chemistry_oxygen.png",
+	
+})
+
+minetest.register_craftitem("chemistry:flourine", {
+	description = "Flourine: 7 VE, 9 P, 10 N",
+	inventory_image = "chemistry_flourine.png",
+	
+})
+
+minetest.register_craftitem("chemistry:neon", {
+	description = "Neon: 8 VE, 10 P, 11 N",
+	inventory_image = "chemistry_neon.png",
+	
+})
